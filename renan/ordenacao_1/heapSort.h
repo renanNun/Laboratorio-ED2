4a1679500c55fe12758169c07f17b3a444700d9c
renan <renannunes400@gmail.com>
2019-08-28 20:25:12 -0300

    Update HeapSort
    O método do victor não dá nenhum erro, ele só não ordena mesmo. Mas ao que parece monta um array legal e troca os lugares

--- renan/ordenacao_1/heapSort.h
+++ renan/ordenacao_1/heapSort.h
@@ -7,27 +7,38 @@ class heapSort{
 public:
     heapSort(){};
     ~heapSort(){};
-    
-    //Está versão apresenta falha de segmentação
-    /*
-    void heapify(int* vetor,int n,int i){
-        int maior = i;
-        int l = 2*i + 1;
-        int r = 2*i +2;
 
-        if(l < n && vetor[l] > vetor[maior]){
-            maior = l;
+    void ordena(int* vet, int n){
+        for(int i = n - 1; i > 1; i--){
+            maxHeapfy(vet,i);
+            troca(vet,0,i);
         }
+    };
 
-        if(r < n && vetor[r] > vetor[maior]){
-            maior = r;
-        }
+    void maxHeapfy(int* vet, int fim){
+        int p = (int) fim/2 - 1;
+        bool chave = true;
 
-        if(maior != i){
-            troca(vetor,i,maior);
-        }
+        while(p > -1){
+            int pai = vet[p];
+            int filho1 = vet[2*p];
+            int filho2 = vet[2*p+1];
 
-        heapify(vetor, n, maior);
+            if(2*p+1 >= fim)
+                chave = false;
+
+            if(filho1 < filho2 && chave){
+                if( pai < filho2){
+                    troca(vet,p,2*p+1);
+                }
+            } else {
+                if(pai < filho1)
+                {
+                    troca(vet,p,2*p);
+                }
+            }
+            p--;
+        }   
     };
 
     void troca(int* vetor, int i, int j){
@@ -35,69 +46,6 @@ public:
         vetor[i] = vetor[j];
         vetor[j] = t;
     };
-
-    void ordena(int* vetor,int n){
-        for(int i = (n / 2) - 1; i >= 0; i--){
-            heapify(vetor,n,i);
-        }
-
-        for(int i = n -1; i >= 0; i--){
-            troca(vetor,0,i);
-
-            heapify(vetor,i,0);
-        }
-    };
-    */
-
-   //Segunda Versão
-
-    //Essa copia da internet não ordena, mas também não faz muita coisa.
-    /*
-    void MaxHeapify(int* vet, int i, int n)
-    {
-	    int j, temp;
-	    temp = vet[i];
-	    j = 2*i;
- 
-     	while (j <= n)
-	    {
-		    if (j < n && vet[j+1] > vet[j])
-		    j = j+1;
-		    // Break if parent value is already greater than child value.
-		    if (temp > vet[j])
-			    break;
-		    // Switching value with the parent node if temp < a[j].
-		    else if (temp <= vet[j])
-		    {
-			    vet[j/2] = vet[j];
-		    	j = 2*j;
-		    }
-	    }
-	    vet[j/2] = temp;
-	    return;
-    }
-
-    void ordena(int* vet, int n)
-    {
-	    int i, temp;
-	    for (i = n; i >= 2; i--)
-	    {
-	    	// Storing maximum value at the end.
-	    	temp = vet[i];
-	    	vet[i] = vet[1];
-	    	vet[1] = temp;
-	    	// Building max heap of remaining element.
-	    	MaxHeapify(vet, 1, i - 1);
-	    }
-    }
-
-    void Build_MaxHeap(int* vet, int n)
-    {
-    	int i;
-    	for(i = n/2; i >= 1; i--)
-    		MaxHeapify(vet, i, n);
-    }
-    */
 
 };
 
