9c04d799e26d98ae19a6cf065a74e31fc0ba39b5
renan <renannunes400@gmail.com>
2019-08-28 18:02:06 -0300

    Update HeapSort.h
    A saga continua irmãos...

--- renan/ordenacao_1/heapSort.h
+++ renan/ordenacao_1/heapSort.h
@@ -7,7 +7,9 @@ class heapSort{
 public:
     heapSort(){};
     ~heapSort(){};
-
+    
+    //Está versão apresenta falha de segmentação
+    /*
     void heapify(int* vetor,int n,int i){
         int maior = i;
         int l = 2*i + 1;
@@ -28,6 +30,11 @@ public:
         heapify(vetor, n, maior);
     };
 
+    void troca(int* vetor, int i, int j){
+        int t = vetor[i];
+        vetor[i] = vetor[j];
+        vetor[j] = t;
+    };
 
     void ordena(int* vetor,int n){
         for(int i = (n / 2) - 1; i >= 0; i--){
@@ -40,20 +47,57 @@ public:
             heapify(vetor,i,0);
         }
     };
+    */
 
-    void troca(int* vetor, int i, int j){
-        int t = vetor[i];
-        vetor[i] = vetor[j];
-        vetor[j] = t;
-    };
+   //Segunda Versão
 
-    void printArray(int arr[], int n) 
-    { 
-    for (int i=0; i<n; ++i) 
-        std::cout << arr[i] << " "; 
-    std::cout << "\n"; 
-    } 
+    //Essa copia da internet não ordena, mas também não faz muita coisa.
+    /*
+    void MaxHeapify(int* vet, int i, int n)
+    {
+	    int j, temp;
+	    temp = vet[i];
+	    j = 2*i;
+ 
+     	while (j <= n)
+	    {
+		    if (j < n && vet[j+1] > vet[j])
+		    j = j+1;
+		    // Break if parent value is already greater than child value.
+		    if (temp > vet[j])
+			    break;
+		    // Switching value with the parent node if temp < a[j].
+		    else if (temp <= vet[j])
+		    {
+			    vet[j/2] = vet[j];
+		    	j = 2*j;
+		    }
+	    }
+	    vet[j/2] = temp;
+	    return;
+    }
 
+    void ordena(int* vet, int n)
+    {
+	    int i, temp;
+	    for (i = n; i >= 2; i--)
+	    {
+	    	// Storing maximum value at the end.
+	    	temp = vet[i];
+	    	vet[i] = vet[1];
+	    	vet[1] = temp;
+	    	// Building max heap of remaining element.
+	    	MaxHeapify(vet, 1, i - 1);
+	    }
+    }
+
+    void Build_MaxHeap(int* vet, int n)
+    {
+    	int i;
+    	for(i = n/2; i >= 1; i--)
+    		MaxHeapify(vet, i, n);
+    }
+    */
 
 };
 
