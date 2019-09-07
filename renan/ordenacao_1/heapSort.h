05047620e121f2e2e51de13f5fd15b550e940a4a
renan <renannunes400@gmail.com>
2019-09-07 09:46:35 -0300

    HeapSort parou de dar erro e eu acho que tá funcional

--- renan/ordenacao_1/heapSort.h
+++ renan/ordenacao_1/heapSort.h
@@ -8,44 +8,58 @@ public:
     heapSort(){};
     ~heapSort(){};
 
-    void ordena(int* vet, int n){
-        for(int i = n - 1; i > 1; i--){
-            maxHeapfy(vet,i);
-            troca(vet,0,i);
+    void troca(int* arr, int i,int j)
+    {
+        int aux = arr[i];
+        arr[i] = arr[j];
+        arr[j] = aux;
+    };
+
+    void heapify(int* arr,int tam,int i){
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if(left < tam && arr[left] > arr[largest])
+        {
+            largest = left;
+        }
+
+        if(right < tam && arr[right] > arr[largest])
+        {
+            largest = right;
+        }
+
+        if(largest != i)
+        {
+            troca(arr,i,largest);
+
+
+            heapify(arr,tam,largest);
         }
     };
 
-    void maxHeapfy(int* vet, int fim){
-        int p = (int) fim/2 - 1;
-        bool chave = true;
+    void buildMaxHeap(int* arr,int tam){
+        int index = (tam / 2) - 1;
 
-        while(p > -1){
-            int pai = vet[p];
-            int filho1 = vet[2*p];
-            int filho2 = vet[2*p+1];
+        for(int i = index; i >= 0; i--)
+        {
+            heapify(arr,tam,i);
+            troca(arr,0,i);
+        }
+    };   
 
-            if(2*p+1 >= fim)
-                chave = false;
-
-            if(filho1 < filho2 && chave){
-                if( pai < filho2){
-                    troca(vet,p,2*p+1);
-                }
-            } else {
-                if(pai < filho1)
-                {
-                    troca(vet,p,2*p);
-                }
-            }
-            p--;
-        }   
+    void printHeap(int* arr,int tam)
+    {
+        std::cout << "Representação de Array Por Heap\n";
+        
+        for(int i = 0; i < tam; i++)
+        {
+            std::cout << arr[i] << " ";
+        }
+        std::cout<<std::endl;
     };
 
-    void troca(int* vetor, int i, int j){
-        int t = vetor[i];
-        vetor[i] = vetor[j];
-        vetor[j] = t;
-    };
 
 };
 
